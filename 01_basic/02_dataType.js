80642b04935481d8e04090b26967fcd1ac01fbeb
S P Mohan Rao <97665630+monurao86866@users.noreply.github.com>
2025-06-20 10:01:05 +0000

    configur done

---
+++ 01_basic/02_dataType.js
@@ -0,0 +1,28 @@
+"use strict"; //to use the new version of JS
+
+//alert(3 + 3) we are using nodejs, not an browser
+
+console.log(3
+
++3
+)  //conde readabilty is important and avoid this way
+
+let name = "Mohan"
+let age = 23
+let isLoggedIn = false
+
+//number +>2 to power 53
+// bigint
+//string => ""
+//boolean => true/false
+// null => standalone value
+//undefined => 
+//symbol => unique
+// the above data type is primitive data type
+
+// non primitive data type 
+//oject 
+
+console.log(typeof null)
+console.log(typeof undefined)
+console.log(typeof Number)
