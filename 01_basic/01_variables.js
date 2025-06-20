42cf59d6f9b564ace1781b3bf491735a31e09d56
S P Mohan Rao <97665630+monurao86866@users.noreply.github.com>
2025-06-20 08:11:19 +0000

    container cofing done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 232232
+let accountEmail = "2323232@gmail.com"
+var accountPassword = "asdasdfa"
+accountCity = "Hyd"
+let accountStage;
+
+// Avoid using Var due to issue with block scope
+// And functional scope 
+
+
+accountEmail + "monurao@gmail.com"
+accountPassword = "sdfasdfsd"
+accountCity = "Banglore"
+accountStage;
+
+console.log([accountEmail, accountPassword, accountCity, accountStage])
+console.table([accountEmail, accountPassword, accountCity, accountStage])
