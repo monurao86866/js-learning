5eaf6f34e33474bf1a691675480b7f4c0d83bce1
S P Mohan Rao <97665630+monurao86866@users.noreply.github.com>
2025-06-24 08:44:27 +0000

    conversion of datatype

---
+++ 01_basic/03_convertionOperation.js
@@ -0,0 +1,33 @@
+let score = "Mohan"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let isScore = Number(score) //converstion to number
+console.log(typeof isScore);
+console.log(isScore);
+
+
+//"23"  => 23
+//"23asdsw" => NaN
+// true => 1; false => 0
+//null => 0
+// undefine => NaN
+
+let isScoreIn = ""
+
+let BooleanisScoreIn = Boolean(isScoreIn) //convertion to bollean
+console.log (BooleanisScoreIn);
+
+//1 => true; 0 => false
+//"" => false
+// "mohan " => True
+
+let someNumber = 23
+
+let someNumberString = String(someNumber)//concvertion to string
+console.log( someNumberString);
+console.log(typeof someNumberString);
+
+
+
